9d296cb7ba6da4d533632cd025f8d7e13e03c7be
Florian Z <64655691+flnzba@users.noreply.github.com>
2022-11-28 23:11:07 +0000

    successfull print of table by selection of <table> tag

--- getdata_bs.py
+++ getdata_bs.py
@@ -6,9 +6,10 @@ url = 'https://www.oetv.at/rangliste/itn.html'
 result = requests.get(url, timeout=20)
 
 site = BeautifulSoup(result.text, "html.parser")
+print(site.table.prettify)
 
-itn = site.find_all(text="ITN")
-parent_itn = itn[0].parent
-print(itn)
+# itn = site.find(text="ITN")
+# parent_itn = itn[0].parent
+# print(site.prettify)
 # header = parent_itn.find("header")
 # print(header.string) #print what was found within the first parent in the header of the table?
